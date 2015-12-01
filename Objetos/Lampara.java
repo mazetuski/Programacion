e14263f069f5b186656a96935518efcdf1097652
mazetuski <migue_95_zb@hotmail.com>
2015-12-01 08:44:43 +0100

    Lampara con objetos

---
+++ Objetos/Lampara.java
@@ -0,0 +1,13 @@
+public class Lampara {
+	boolean encendida = false;
+
+	void encender() {
+		encendida = true;
+		System.out.println("Ilumninando");
+	}
+
+	void apagar() {
+		encendida = false;
+		System.out.println("Oscuridad");
+	}
+}
---
+++ Objetos/TestLampara.java
@@ -0,0 +1,8 @@
+
+public class TestLampara {
+	public static void main(String[] args) {
+	Lampara lampara=new Lampara(); //instancia un objeto
+	lampara.encender(); // invoca un metodo
+	lampara.apagar();
+	}
+}
